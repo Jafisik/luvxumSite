63831435f8e36a297f4ea89cdf441c64e9f9c539
Jafisik <jafisik@gmail.com>
2024-09-25 03:42:05 +0200

    Oprava animaci snad a struktury

--- Luvxum/script.js
+++ Luvxum/script.js
@@ -3,6 +3,7 @@ const menuItems = document.querySelectorAll('.menu ul li');
 const logo = document.getElementById('logo');
 const menu = document.querySelector('.menu');
 const back = document.getElementById('back');
+back.style.display = 'none';
 
 const luvxum = document.getElementById('luvxum');
 const luvxumContent = document.getElementById('luvxumContent');
@@ -11,45 +12,65 @@ const photoContent = document.getElementById('photoContent');
 const illu = document.getElementById('illu');
 const illuContent = document.getElementById('illuContent');
 const content = document.getElementById('content');
+luvxumContent.style.display = 'none';
+photoContent.style.display = 'none';
+illuContent.style.display = 'none';
 
 // Přidáme event listener pro každou položku v menu
 menuItems.forEach(item => {
     item.addEventListener('click', () => {
-        // Fade out logo
         logo.classList.add('fade');
-
-        menu.querySelector('ul').classList.add('horizontal');
+        menu.classList.add('up');
         back.style.display = 'block';
-        menu.style.top = '0'; // Posune menu na vrchol stránky
+        menu.style.top = '0';
+        content.classList.add('contentIn');
+        if(item.id != 'back'){
+            setTimeout(() => {
+                logo.style.display = 'none';
+              }, 200);
+        }
     });
 });
 
 back.addEventListener('click', () => {
-    logo.classList.remove('fade', 'fadeBack');
-    menu.querySelector('ul').classList.remove('horizontal', 'back');
+    logo.style.display = 'block';
+    logo.classList.remove('fade');
+    menu.classList.remove('up');
     back.classList.remove('back');
+    content.classList.remove('contentIn');
+    luvxum.style.color = 'white';
+    photo.style.color = 'white';
+    illu.style.color = 'white';
     back.style.display = 'none';
     luvxumContent.style.display = 'none';
     photoContent.style.display = 'none';
-    illuContent.style.display = 'none'
-    content.style.display = 'none';
+    illuContent.style.display = 'none';
 });
 
 luvxum.addEventListener('click', () => {
+    luvxum.style.color = 'red';
+    photo.style.color = 'white';
+    illu.style.color = 'white';
     content.style.display = 'block';
     luvxumContent.style.display = 'block';
     photoContent.style.display = 'none';
-    illuContent.style.display = 'none'
+    illuContent.style.display = 'none';
 });
 photo.addEventListener('click', () => {
+    luvxum.style.color = 'white';
+    photo.style.color = 'red';
+    illu.style.color = 'white';
     content.style.display = 'block';
     luvxumContent.style.display = 'none';
     photoContent.style.display = 'block';
-    illuContent.style.display = 'none'
+    illuContent.style.display = 'none';
 });
 illu.addEventListener('click', () => {
+    luvxum.style.color = 'white';
+    photo.style.color = 'white';
+    illu.style.color = 'red';
     content.style.display = 'block';
     luvxumContent.style.display = 'none';
     photoContent.style.display = 'none';
-    illuContent.style.display = 'block'
+    illuContent.style.display = 'block';
 });
